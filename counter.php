79791f945b9eefc73f5e5d855b6de32293574cfb
tallmantim <tallmantim@gmail.com>
2020-01-07 07:46:03 +0000

    updated

--- counter.php
+++ counter.php
@@ -1,12 +1,7 @@
 <?php
 session_start();
-$counter = intval($_SESSION["counter"] );
-
-$counter ++ ;
-
-$_SESSION["counter"]=$counter;
 
 $name = shell_exec('cat /proc/self/cgroup | grep "pids:/docker"| cut -f 3 -d/');
 
-echo $name." ".$counter."\n";
+echo $name."\n";
 ?>
